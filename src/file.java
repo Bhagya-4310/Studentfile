d25a6ce4a526f89a7e203217fe81585259cf993b
Bhagya-4310 <157447593+Bhagya-4310@users.noreply.github.com>
2024-02-11 12:54:51 +0530

    file added

--- src/file.java
+++ src/file.java
@@ -1,3 +1,13 @@
 public class file {
     public String name;
+    public String fatherName;
+    if(String name.equals('bhagi') && String fatherName.equals('amar'))
+    {
+        System.out.println("informaton is correct");
+    }
+    else
+    {
+        System.out.println("information is wrong");
+    }
+
 }
